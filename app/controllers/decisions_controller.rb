088e5b0acbde2fd494aed5eb993667906b180fa4
Santiago Ferrari <113905797+sferrari91@users.noreply.github.com>
2022-12-15 13:06:06 +0100

    general logic of swipe right done

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,31 +1,53 @@
 class DecisionsController < ApplicationController
-  before_action :decision_check?, only: %i[swipe_right]
 
   def new
     @decision = Decision.new
   end
 
-  def create
+  def create_right
     @decision = Decision.new
     @decision.user = current_user
-    @decision.user2 = User.find(params[:user])
+    @decision.user_2 = params[:user]
+    @decision.likes = true
     @decision.save!
+    raise
   end
 
   def swipe_right
     if decision_check?
-      update
+      update_right
     else
-      create
+      create_right
     end
   end
 
-  def update
+  def swipe_left
+    if decision_check?
+      update_left
+    else
+      create_left
+    end
+  end
+
+  def update_right
     @decision = Decision.where(["user_id = ? and user_2 = ?", "#{params[:user]}", "#{current_user}"])
     @decision.likes2 = true
     if @decision.likes == true && @decision.likes2 == true
       create_match
     end
+  end
+
+  def create_left
+    @decision = Decision.new
+    @decision.user = current_user
+    @decision.user2 = User.find(params[:user])
+    @decision.likes = false
+    @decision.save!
+  end
+
+  def update_left
+    @decision = Decision.where(["user_id = ? and user_2 = ?", "#{params[:user]}", "#{current_user}"])
+    @decision.likes2 = false
   end
 
   def create_match
@@ -37,7 +59,7 @@ class DecisionsController < ApplicationController
   private
 
   def decision_check?
-    @decision = Decision.where(["user_id = ? and user_2 = ?", "#{params[:user]}", "#{current_user}"])
+    @decision = Decision.where(["user_id = ? and user_2 = ?", "#{params[:user]}", "#{current_user.id}"])
     @decision.present?
   end
 
