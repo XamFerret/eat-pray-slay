afd7763884e64a9669693c6f17722ac3b2d1a93c
Santiago Ferrari <113905797+sferrari91@users.noreply.github.com>
2022-12-16 14:46:33 +0100

    omg im in master again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,19 @@ class PagesController < ApplicationController
     @decisions = Decision.where(user_id: current_user)
   end
 
+  def next_user
+    @decisions = Decision.where(user_id: current_user)
+    @decisions.each do |decision|
+      
+    end
+  end
+
+  <% @decisions.each do |decision| %>
+    <% user_2 << decision.user_2 %>
+    <% all_users = User.where.not(id: user_2).and(User.where.not(id: current_user.id)) %>
+    <% @user_decision = all_users.sample%>
+  <% end %>
+
   def matches
     @decisions = Decision.where("user_id = ? or user_2 = ?", "#{current_user.id}", "#{current_user.id}")
     @matches = @decisions.where("likes = ? and likes2 = ?", "true", "true")
